33fdcdf55e2854473dc77395541687257bc2093c
vaishakkrishna <vaishak.krishna@berkeley.edu>
2023-08-15 23:15:54 -0400

    startup change

--- startup-scripts/startup.sh
+++ startup-scripts/startup.sh
@@ -1,3 +1,3 @@
 docker container prune -f
 docker run --name inference --gpus '"device=0"' -p 8000:80 -i -t -d -v vllm:/mnt/inference --shm-size=16g nvcr.io/nvidia/pytorch:22.12-py3
-docker exec -i -t -d -w /mnt/inference/calibration-scripts inference /bin/bash -c "git pull && source env/bin/activate && python run_calibrations.py --llm_name baichuan-inc/Baichuan-13B-Chat --gpus 1 --batch_size 512"
+docker exec -i -t -d -w /mnt/inference/calibration-scripts inference /bin/bash -c "git pull && source env/bin/activate && python run_calibrations.py --llm_name baichuan-inc/Baichuan-13B-Base --gpus 1 --batch_size 512"
