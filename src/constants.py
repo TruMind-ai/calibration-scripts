8566ac5475a66c994434dcd7da3b4c7e00323f4f
vaishakkrishna <vaishak.krishna@berkeley.edu>
2023-08-22 12:58:18 -0400

    added llama 2 base model

--- src/constants.py
+++ src/constants.py
@@ -3,12 +3,14 @@ BAICHUAN = 'baichuan-inc/Baichuan-13B-Base'
 L2_ORCA = 'OpenAssistant/llama2-13b-orca-8k-3319'
 L2_OPENCHAT = 'openchat/openchat_v3.1'
 L2_VOICELAB = 'Voicelab/trurl-2-13b'
+L2 = 'meta-llama/Llama-2-13b-hf'
+L2_ORCA_CIRCULUS = 'circulus/Llama-2-13b-orca-v1'
 
 
 ALL_DIMENSIONS = [ 'authority', 'consensus',
               'consistency', 'scarcity', 'unity', 'liking', 'reciprocity']
 
-ALL_LLMS = [BAICHUAN, L2_OPENCHAT, L2_OPENCHAT, L2_VOICELAB]
+ALL_LLMS = [BAICHUAN, L2_OPENCHAT, L2_OPENCHAT, L2_VOICELAB, L2]
 
 LLM_ABBR_MAP = {
     'OpenAssistant/llama2-13b-orca-8k-3319': 'BAICHUAN',
@@ -40,5 +42,7 @@ LLM_TEMPLATES_V2 = {
     'OpenAssistant/llama2-13b-orca-8k-3319': DEFAULT_TEMPLATE_V2,
     'openchat/openchat_v3.1': DEFAULT_TEMPLATE_V2,
     'augtoma/qCammel-70-x': VICUNA_TEMPLATE_V2,
-    L2_VOICELAB: VOICELAB_TEMPLATE
+    L2_VOICELAB: VOICELAB_TEMPLATE, 
+    L2: DEFAULT_TEMPLATE_V2,
+    L2_ORCA_CIRCULUS: DEFAULT_TEMPLATE_V2
 }
