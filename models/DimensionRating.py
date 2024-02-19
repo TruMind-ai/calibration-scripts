4ce279f16c2338dde828300174a616ce9d141f63
Vaishak Krishna <vaishak.krishna@berkeley.edu>
2024-02-19 15:13:03 -0800

    updated dimension rating

--- models/DimensionRating.py
+++ models/DimensionRating.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict, List
 from pydantic import BaseModel, ConfigDict, Field
 
 from .utils import PyObjectId
@@ -13,3 +14,7 @@ class DimensionRating(BaseModel):
     sample_index: int = -1
     prompt_index: int = -1
     prefix_index: int = -1
+
+    @staticmethod
+    def from_list(dr_list=List[Dict[str, Any]]) -> List:
+        return [DimensionRating(**d) for d in dr_list]
