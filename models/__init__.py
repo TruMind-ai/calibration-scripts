9ddc085eff9056a8e534aa3261728c5303c9664f
Vaishak Krishna <vaishak.krishna@berkeley.edu>
2024-02-19 14:12:01 -0800

    dep fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-from .DimensionMetadata import DimensionMetadata, DimensionStatus
+from .DimensionMetadata import DimensionMetadata
 from .LLMProgress import LLMProgress
 from .DimensionRating import DimensionRating
 from .DimensionAsset import DimensionAsset, DimensionAssetType
